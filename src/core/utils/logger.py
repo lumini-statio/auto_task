29caff4fd69ed912c83200b0ec5bcebbd7466691
Emilio Luna <lunaemilio2003@gmail.com>
2025-02-28 18:26:45 -0300

    #2 new logger function and traceback imports

---
+++ src/core/utils/logger.py
@@ -0,0 +1,19 @@
+import os
+import logging
+
+appdata_path = os.getenv('APPDATA')
+logs_dir = os.path.join(appdata_path, 'automation', 'logs')
+
+os.makedirs(logs_dir, exist_ok=True)
+
+log_file = os.path.join(logs_dir, 'automation.log')
+
+logging.basicConfig(
+    filename=log_file,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S'
+)
+
+def log(message: str):
+    logging.info(message)
--- src/core/utils/repeated_files.py
+++ src/core/utils/repeated_files.py
@@ -1,46 +1,47 @@
 import os
 import hashlib
-from pprint import pprint
+from logger import log
+import traceback
 
 
 repeated: dict = {}
     
     
 def hash_file(filename, *args):
-        hash = hashlib.md5()
+    hash = hashlib.md5()
 
-        try:
-            with open(filename, 'rb') as file:
-                while chunk := file.read(8192):
-                    hash.update(chunk)
-            return hash.hexdigest()
-        except (OSError, PermissionError) as e:
-            print(f"Error al procesar {filename}: {e}")
-        return None
+    try:
+        with open(filename, 'rb') as file:
+            while chunk := file.read(8192):
+                hash.update(chunk)
+        return hash.hexdigest()
+    except (OSError, PermissionError):
+        log(f'{__file__} - {traceback.format_exc()}')
+    return None
         
     
 def delete_repeated(path: str):
-    for root, dirs, files in os.walk(path):
-        dirs[:] = [
-            d for d in dirs 
-            if not d.startswith('.') 
-            and d not in ['AppData', 'OpenVPN']
-        ]
-        for file in files:
-            if not file.endswith(('.DAT', '.LOG1', '.LOG2')):
-                full_path = os.path.join(root, file)
-                print(full_path)
-                file_hash = hash_file(full_path)
-            
-                if file_hash is None:
-                    continue
+    try:
+        for root, dirs, files in os.walk(path):
+            dirs[:] = [
+                d for d in dirs 
+                if not d.startswith('.') 
+                and d not in ['AppData', 'OpenVPN']
+            ]
+            for file in files:
+                if not file.endswith(('.DAT', '.LOG1', '.LOG2')):
+                    full_path = os.path.join(root, file)
+                    file_hash = hash_file(full_path)
                 
-                if file_hash in repeated:
-                    os.remove(full_path)
-                    print(f"REMOVED")
-                else:
-                    repeated[file_hash] = full_path
-                    print(f"ADDED: {full_path}")
+                    if file_hash is None:
+                        continue
+                    
+                    if file_hash in repeated:
+                        os.remove(full_path)
+                    else:
+                        repeated[file_hash] = full_path
+    except Exception:
+        log(f'{__file__} - {traceback.format_exc()}')
 
 
 def main(user_name: str):
